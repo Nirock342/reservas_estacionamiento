994d4bc583772178bd57a4b6f5087d0b5747d2d4
Nirock342 <nirock6@gmail.com>
2023-05-24 19:47:50 -0500

    calendario para recerva interactivo añadido

--- reservas/forms.py
+++ reservas/forms.py
@@ -1,23 +1,13 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
 from .models import Reserva
+from django import forms
+
 
 
 class ReservaForm(forms.ModelForm):
-    fecha_reservada = forms.DateTimeField(
-        widget=DatePicker(
-            options={
-                'format': 'YYYY-MM-DD',
-                'useCurrent': True,
-                'Collapse': False,
-            }
-        ),
-        input_formats=[
-            '%Y-%m-%d',       # Formato YYYY-MM-DD
-            '%d/%m/%Y',       # Formato DD/MM/YYYY
-            '%m/%d/%Y',       # Formato MM/DD/YYYY
-            '%Y/%m/%d',       # Formato YYYY/MM/DD
-        ]
+    fecha_reservada = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'})
     )
     
     class Meta:
